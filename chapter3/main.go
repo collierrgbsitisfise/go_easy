b207990c1b935b5b831c1620d094d0d9179f7df2
Vadim Nicolaev <vadim.vicolaev.v@gmail.com>
2018-07-03 22:25:36 +0300

    variables and input

---
+++ chapter3/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(len("Hello world"))
+	fmt.Println("Hello world"[1])
+	fmt.Println("Hello " + "World")
+	fmt.Println("1 + 1 = ", 1 + 1)
+}
