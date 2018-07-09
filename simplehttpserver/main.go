0142cde51b331bb1863fecfb629e9efb9d59ffac
Vadim Nicolaev <vadim.vicolaev.v@gmail.com>
2018-07-09 22:47:59 +0300

    implent generation of lissajous git for handler server

--- simplehttpserver/main.go
+++ simplehttpserver/main.go
@@ -2,9 +2,24 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
 	"log"
+	"math"
+	"math/rand"
 	"net/http"
+	"strconv"
 	"sync"
+	"time"
+)
+
+var palette = []color.Color{color.White, color.Black}
+
+const (
+	whiteIndex = 0
+	blackIndex = 1
 )
 
 var mu sync.Mutex
@@ -13,7 +28,19 @@ var count int
 func main() {
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/count", counter)
+	http.HandleFunc("/lissajous", getLissajousAnimationHandler)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+func getLissajousAnimationHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get cycle params")
+	cycleParam := r.URL.Query().Get("cycles")
+	cycles, err := strconv.Atoi(cycleParam)
+
+	if err != nil {
+		cycles = 20
+	}
+
+	lissajous(w, float64(cycles))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -27,4 +54,31 @@ func counter(w http.ResponseWriter, r *http.Request) {
 	mu.Lock()
 	fmt.Fprintf(w, "Count %d\n", count)
 	mu.Unlock()
+}
+
+func lissajous(out io.Writer, cycles float64) {
+	const (
+		// cycles  = 5
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+	rand.Seed(time.Now().UTC().UnixNano())
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
 }
