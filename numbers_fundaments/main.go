0b731e2c76d80eaa8f5bd7c981a5d4529b2f4b80
Vadim Nicolaev <vadim.vicolaev.v@gmail.com>
2018-07-12 21:44:13 +0300

    implement handler to get svg

--- numbers_fundaments/main.go
+++ numbers_fundaments/main.go
@@ -17,7 +17,8 @@ const (
 var sin30, cos30 = math.Sin(angel), math.Cos(angel)
 
 func main() {
-	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
+	var res string
+	res += fmt.Sprintf("<svg xmlns='http://www.w3.org/2000/svg' "+
 		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
 		"width='%d' height='%d' >", width, height)
 	for i := 0; i < cells; i++ {
@@ -26,10 +27,11 @@ func main() {
 			bx, by := corner(i, j)
 			cx, cy := corner(i, j+1)
 			dx, dy := corner(i+1, j+1)
-			fmt.Printf("<polygon points='%g,%g,%g,%g,%g,%g,%g,%g' />\n", ax, ay, bx, by, cx, cy, dx, dy)
+			res += fmt.Sprintf("<polygon points='%g,%g,%g,%g,%g,%g,%g,%g' />\n", ax, ay, bx, by, cx, cy, dx, dy)
 		}
 	}
-	fmt.Println("</svg>")
+	res += fmt.Sprintf("</svg>")
+	fmt.Println(res)
 }
 
 func corner(i, j int) (float64, float64) {
